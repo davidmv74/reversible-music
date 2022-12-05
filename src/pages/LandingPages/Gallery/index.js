9c05fa59aa1a4c9d1c0c49681921d0bd36a21f26
davidmv74 <davidmv74@gmail.com>
2022-12-04 22:07:10 -0500

    Adding basic gallery and text

--- src/pages/LandingPages/Gallery/index.js
+++ src/pages/LandingPages/Gallery/index.js
@@ -2,13 +2,9 @@
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 
-
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
 import ImageCard from "examples/Cards/BlogCards/ImageCard";
-
-import FilledInfoCard from "examples/Cards/InfoCards/FilledInfoCard";
-
 
 // Material Kit 2 React example components
 import DefaultNavbar from "examples/Navbars/DefaultNavbar";
@@ -20,17 +16,24 @@ import routes from "routes";
 import bgImage from "assets/images/bg-presentation.jpg";
 
 function Gallery() {
-
+  const driveImageIDs = [
+    "15PJPfzzmptBf_j_8GHkb5bfs9msTKDcp",
+    "185hKti02Eth-QbBxfPvFPo2FIuBT7Wzy",
+    "1nA-hlay8gO4N6qI1LuiN1DuK-_bcRnKw",
+    "173Li9Q0vwP0yZnQzkwID7mC1Xj9KfAB-",
+    "16vorSqgYUOA8hYCS5jLF_ojBsQr60_yi",
+    "16n3vR4tBhXyDWNe8dxK4cbUZW0oGAewP",
+    "16gDJEJ4AOgVkjn27UWjJ2F2bskl7d0O9",
+    "1zLfo26JW4Xzp7-VxIolE2kg4rwgAxX-M",
+    "1iJSlTLMOvpar-Z6LXQAN_aAhrhzNAvDB"
+  ];
   return (
     <>
-      <DefaultNavbar
-        routes={routes}
-        transparent
-        light
-      />
+      <DefaultNavbar routes={routes} transparent light />
       <MKBox
         minHeight="100vh"
         width="100%"
+        pt={"8%"}
         sx={{
           backgroundImage: `url(${bgImage})`,
           backgroundSize: "cover",
@@ -40,52 +43,18 @@ function Gallery() {
         }}
       >
         <Container>
-        <Grid container spacing={3}>
-            <Grid item xs={12} lg={4}>
-              <FilledInfoCard
-                variant="gradient"
-                color="info"
-                icon="flag"
-                title="Getting Started"
-                description="Check the possible ways of working with our product and the necessary files for building your own project."
-                action={{
-                  type: "external",
-                  route: "https://www.creative-tim.com/learning-lab/react/overview/material-kit/",
-                  label: "Let's start",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} lg={4}>
-              <FilledInfoCard
-                color="info"
-                icon="precision_manufacturing"
-                title="Plugins"
-                description="Get inspiration and have an overview about the plugins that we used to create the Material Kit."
-                action={{
-                  type: "external",
-                  route: "https://www.creative-tim.com/learning-lab/react/overview/datepicker/",
-                  label: "Read more",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} lg={4}>
-              <FilledInfoCard
-                color="info"
-                icon="apps"
-                title="Components"
-                description="Material Kit is giving you a lot of pre-made components, that will help you to build UI's faster."
-                action={{
-                  type: "external",
-                  route: "https://www.creative-tim.com/learning-lab/react/alerts/material-kit/",
-                  label: "Read more",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} lg={4}>
-              <ImageCard
-                image="https://images.unsplash.com/photo-1544717302-de2939b7ef71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-              />
-            </Grid>
+          <Grid container spacing={3}>
+            {driveImageIDs.map((imageID) => {
+              return (
+                <Grid item xs={12} lg={4}>
+                  <ImageCard
+                    image={
+                      "https://drive.google.com/uc?export=view&id=" + imageID
+                    }
+                  />
+                </Grid>
+              );
+            })}
           </Grid>
         </Container>
       </MKBox>
--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -41,8 +41,8 @@ function Presentation() {
             <MKTypography
               variant="h1"
               color="white"
-              mt="40%"
-              mb={1}
+              mt="15%"
+              mb="85%"
               sx={({ breakpoints, typography: { size } }) => ({
                 [breakpoints.down("md")]: {
                   fontSize: size["3xl"],
@@ -51,14 +51,87 @@ function Presentation() {
             >
               Reversible Music
             </MKTypography>
+          </Grid>
+        </Container>
+      </MKBox>
+      <MKBox
+        minHeight="100vh"
+        width="100%"
+        bgColor="black"
+      >
+        <Container>
+          <Grid container item xs={10} lg={12} justifyContent="center" mx="auto">
             <MKTypography
-              variant="h3"
-              color="white"
-              textAlign="center"
-              px={{ xs: 6, lg: 12 }}
-              mt={1}
+              variant="p"
+              fontWeight="medium"
+              color="light"
+              mt={"8%"}
+              mb={4}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
             >
-              Coming Soon...
+              {'Reversible is a progressive band functioning as an experimental playground of sound creation. '
+              +'Their EP, Answers, aims to be an eclectic hybridization of various genre elements and styles of origins. '
+              +'Textures from 70s rock and the best of contemporary progressive rock, jazz, ambient and spatial sounds, '
+              +'electronic, drum and bass, and even ethnic music join together without prejudice in our proposal.'} 
+            </MKTypography>
+
+            <MKTypography
+              variant="p"
+              fontWeight="medium"
+              color="light"
+              mb={4}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              {'As such, the listener will notice details uncovering the intentions of the band, totally atypical in the present cuban musical landscape. '
+              +'This feeling of collage, experimentation, and richness in the use of multiple sounds are enhanced by a wide array of colors and virtues '
+              +'in the motivations and origins of each participating musician (Marco Alonso, William Pacheco, Sandor Rain-Hill, Jose Bermudez, and Jose Machado) '
+              +'— this is exactly what contributes to the classification of Reversible’s proposal under the category of progressive music.'} 
+            </MKTypography>
+            <MKTypography
+              variant="p"
+              fontWeight="medium"
+              color="light"
+              mb={4}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              {'In Reversible’s recordings for their first work attention is drawn to the use of jazzy bases, with keyboards and synthesizers '
+              +'in which textures from the 70s are predominant and progressive sounds interact in perfect communion with those of classical upright pianos. '
+              +'At the same time, one can appreciate experimental flirtation and acidic touch of the guitars that manage to go beyond a conventional rock discourse. '
+              +'The drums, always far from compressions popular today, produce a natural sound with occasional processing through various effects '
+              +'that aim to convert them into another melodic instrument. The coming together of electronic instruments with acoustic ones like the saxophone, contrabass, and piano '
+              +'creates an atmosphere open to the assimilation of multiple influences which breaks formulaic schematism. '
+              +'This culminates with the accomplishment that when listening to the disk Walking Blind one receives it as a contemporary and modern work '
+              +'that is at the same time characterized by warm, vintage sounds.'} 
+            </MKTypography>
+            <MKTypography
+              variant="p"
+              fontWeight="medium"
+              color="light"
+              mb={4}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              {'Reversible’s intentions, however, don’t stop there. Conceptually it can be said that, given the breathable spirituality throughout the 10 pieces in the album '
+              +'— produced by Marco Alonso and William Pacheco — there also is an approximation to world music, if perceived in the broadest of terms. '
+              +'In parallel, the spirituality of the disk makes it a psychedelic journey, a sort of dream or trance, with a transcendental and all-encompassing '
+              +'cosmic vision of life beyond death, all associated with the nocturnal and bohemian. Therein lies another reason for the variety of textures, '
+              +'environmental and psychedelic charge transmitted by the phonogram as a whole. If you are music lover open to enjoying proposals that break with the conventional '
+              +'tight compartments, I assure you that Walking Blind as the first work of Reversible, a disk that looks to expand the existing stylistic limits of specific musical genres, will not disappoint.'} 
             </MKTypography>
           </Grid>
         </Container>
